cc2ff421a67a74469013cd1f361384b29ef0af91
Luca <luca.cavagnoli@gmail.com>
2023-01-29 13:42:18 +0100

    Void > void

--- src/main/java/com/example/cleanarchitecture/UseCaseTemplate.java
+++ src/main/java/com/example/cleanarchitecture/UseCaseTemplate.java
@@ -4,29 +4,20 @@ import com.example.cleanarchitecture.exceptions.BusinessValidationException;
 import com.example.cleanarchitecture.exceptions.RequestValidationException;
 import com.example.monads.Try;
 
-public abstract class UseCaseTemplate<
-    I extends UseCaseRequest<?>,
-    O extends UseCaseResponse<?>
->
-    implements UseCase<I, O> {
+public abstract class UseCaseTemplate<I extends UseCaseRequest<?>, O extends UseCaseResponse<?>>
+        implements UseCase<I, O> {
 
     @Override
     public final Try<O> handle(I request) {
         return Try
-                .ofFailable(() -> {
-                    validateRequest(request);
-                    return null;
-                })
-                .map(x -> {
-                    validateBusiness(request);
-                    return null;
-                })
+                .ofFailable(() -> validateRequest(request))
+                .map(x -> validateBusiness(request))
                 .map(x -> work(request));
     }
 
-    protected abstract void validateRequest(I request) throws RequestValidationException;
+    protected abstract Void validateRequest(I request) throws RequestValidationException;
 
-    protected abstract void validateBusiness(I request) throws BusinessValidationException;
+    protected abstract Void validateBusiness(I request) throws BusinessValidationException;
 
     protected abstract O work(I request);
 }
