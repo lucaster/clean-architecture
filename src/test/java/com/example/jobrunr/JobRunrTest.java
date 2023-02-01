3278c855ee929f5924c3dddc875d48ed4e96d543
Luca <luca.cavagnoli@gmail.com>
2023-02-01 22:18:09 +0100

    does not work at all

--- src/test/java/com/example/jobrunr/JobRunrTest.java
+++ src/test/java/com/example/jobrunr/JobRunrTest.java
@@ -2,8 +2,10 @@ package com.example.jobrunr;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.UUID;
 
 import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.jobs.details.JobDetailsAsmGenerator;
 import org.jobrunr.scheduling.BackgroundJob;
 import org.jobrunr.storage.InMemoryStorageProvider;
 import org.junit.Test;
@@ -13,20 +15,21 @@ public class JobRunrTest {
     @Test
     public void doesNotWork() throws Exception {
 
-        JobRunr.configure()
-        // .useJobActivator(applicationContext::getBean)
-        .useStorageProvider(new InMemoryStorageProvider())
-        .useBackgroundJobServer()
-        .initialize();
+       JobRunr.configure()
+       // .useJobActivator(applicationContext::getBean)
+       .useStorageProvider(new InMemoryStorageProvider())
+       .useJobDetailsGenerator(new JobDetailsAsmGenerator())
+       .useBackgroundJobServer()
+       .initialize();
 
-        BackgroundJob.enqueue(() -> System.out.println("%s enqueue".formatted(Instant.now())));
+       BackgroundJob.enqueue(() -> System.out.println("%s enqueue".formatted(Instant.now())));
 
-        BackgroundJob.schedule(Instant.now().plusMillis(500), () -> System.out.println("%s schedule".formatted(Instant.now())));
+       BackgroundJob.schedule(UUID.randomUUID(), Instant.now().plusMillis(500), () -> System.out.println("%s schedule".formatted(Instant.now())));
 
-        BackgroundJob.scheduleRecurrently(Duration.ofSeconds(5), () -> System.out.println("%s scheduleRecurrently(Duration)".formatted(Instant.now())));
+       BackgroundJob.scheduleRecurrently("a", Duration.ofSeconds(5), () -> System.out.println("%s scheduleRecurrently(Duration)".formatted(Instant.now())));
 
-        BackgroundJob.scheduleRecurrently("*/5 * * * * *", () -> System.out.println("%s scheduleRecurrently(Cron)".formatted(Instant.now())));
+       BackgroundJob.scheduleRecurrently("b", "*/5 * * * * *", () -> System.out.println("%s scheduleRecurrently(Cron)".formatted(Instant.now())));
 
-        Thread.sleep(12000);
+       Thread.sleep(1000);
     }
 }
